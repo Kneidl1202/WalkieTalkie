1cc43a49de2f1a58dc885f36305ec25a095630f6
Kneidl1202 <andreas.auer03@gmail.com>
2020-12-09 11:25:55 +0100

    enoough for today. found some stuff for the display. missing library for TFT.h.

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,6 +1,17 @@
 #include <Arduino.h>
 #include "ezButton.h"
 
+//display
+#include <TFT.h>
+#include <SPI.h>
+
+#define cs 10
+#define dc 9
+#define rst 8
+
+TFT TFTscreen = TFT(cs, dc, rst);
+
+//microphone
 ezButton button(7);
 bool RA = true;
 
@@ -10,17 +21,41 @@ void readOut();
 void setup()
 {
   Serial.begin(9600);
+
+  //microphone
   button.setDebounceTime(50);
+
+  //display
+  TFTscreen.begin();
+  TFTscreen.background(0, 0, 0);
+  TFTscreen.setTextSize(2);
 }
 
-
 void loop()
-{   
-    if(button.isPressed()){
-      RA = !RA;
-    }
+{
+  //microphone
+  if (button.isPressed())
+  {
+    RA = !RA;
+  }
 
-    readOut();
-    setRA(RA);
-    delay(1000);
+  //readOut();
+  //setRA(RA);
+  //delay(1000);
+
+  //display
+
+  //generate a random color
+  int redRandom = random(0, 255);
+  int greenRandom = random(0, 255);
+  int blueRandom = random(0, 255);
+
+  // set a random font color
+  TFTscreen.stroke(redRandom, greenRandom, blueRandom);
+
+  // print Hello, World! in the middle of the screen
+  TFTscreen.text("Hello, World!", 6, 57);
+
+  // wait 200 miliseconds until change to next color
+  delay(200);
 }
