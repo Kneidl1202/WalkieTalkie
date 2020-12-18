496d849b70e022043a1766a58807e97932929604
Kneidl1202 <andreas.auer03@gmail.com>
2020-12-18 18:34:15 +0100

    finished base code for the Funktransmitter

--- Code/src/funkTransmitter.cpp
+++ Code/src/funkTransmitter.cpp
@@ -1,39 +1,54 @@
 //here will be the code for the funk module
 
 #include <SoftwareSerial.h>
+#include <global.h>
 
 SoftwareSerial HC12(10, 11); //HC-12 TX-PIN, HC-12 RX-PIN
 
 void InitFunkTransmitter()
 {
     HC12.begin(9600);
-}
 
-int MainFunkTransmitter(int txd[] = {})
-{
-    //call first TXD and send datas. then read datas and return them.
-    return 0;
+    int i;
+
+    for (i = 0; i < 8; ++i)
+    {
+        txd[i] = 0;
+    }
 }
 
 //TODO sendign is an Array if I'm right
-void TXD(int txd[])
+void TXD()
 {
     int i;
-    for (i = 0; i < int(sizeof(txd)); ++i) //TODO not sure if it does what it has to
+
+    for (i = 0; i < 8; ++i) //TODO not sure if it does what it has to do
     {
         HC12.write(txd[i]); // Send that data to HC-12
     }
+
+    for (i = 0; i < 8; ++i)
+    {
+        txd[i] = 0;
+    }
 }
 
-//TODO return should be a array if I'm right
-int RXD()
+//TODO return should be a array if I'm right. create an array in global.h and use that one to store the read dataset.
+void RXD()
 {
-    int ret = 0;
+    int i = 0;
 
     while (HC12.available())
     {
-        ret = HC12.read(); // return the data to Serial monitor
+        rxd[i] = HC12.read(); // return the data to MainFunkTransmitter
+        ++i;
     }
+}
 
-    return ret;
+//main Function for Funk Transmitter. get called every loop round. sends data over and recieves data.
+void MainFunkTransmitter()
+{
+    //call first TXD and send datas. then read datas
+    TXD();
+    RXD();
 }
