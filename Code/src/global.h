d416e338b215e9126d17c80a8341827a0a3964d0
Kneidl1202 <andreas.auer03@gmail.com>
2020-12-18 18:33:29 +0100

    add init.h and global.h. basically two includes which contain global variables and where the init and main methods are include.

---
+++ Code/src/global.h
@@ -0,0 +1,7 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern int txd[8];
+extern int rxd[8];
+
+#endif
---
+++ Code/src/init.h
@@ -0,0 +1,12 @@
+#ifndef INIT_H
+#define INIT_H
+
+void InitFunkTransmitter();
+void InitDisplay();
+void InitMicrophone();
+
+void MainFunkTransmitter();
+void MainDisplay();
+void MainMicrophone();
+
+#endif
